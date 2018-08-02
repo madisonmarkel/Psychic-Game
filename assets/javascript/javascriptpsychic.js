b85f969cfe7c6e06da3098fbc480478473583ac9
Madison Markel <madisonmarkel@Maddie-Laptop.local>
2018-08-01 20:35:28 -0600

    working gamegit add -A

--- assets/javascript/javascriptpsychic.js
+++ assets/javascript/javascriptpsychic.js
@@ -1,10 +1,19 @@
 //Computer Psychic Guesses
 var psychicGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Arrays
+//Global Arrays
 var matches = 0;
 var losses = 0;
-var guessesLeft = 10;
+var wrongGuess = [""];
+var guessesLeft=10;
+
+//Reset Function
+function reset() {
+    guessesLeft=10;
+    wrongGuess = [""];
+    matches=0;
+    losses=0;
+}
 
 //User presses a key
 document.onkeyup = function(event) {
@@ -12,35 +21,39 @@ document.onkeyup = function(event) {
     //determines what key is pressed
     var userGuess = event.key;
 
-    //How pyschic chooses a letter
+    //How pyschic chooses a random letter
     var psychicChoice = psychicGuesses[Math.floor(Math.random() * psychicGuesses.length)];
-
-        // need a loop
-       // for (var i=0; i<guessesLeft; i++) {
-            //if statements
+ //create something that rejects wrong type guesses
+        //loop
+       for (var i=0; i < 10; i++); {
+            //if statements that run game
             if (userGuess === psychicChoice) {
                 matches++;
             } else {
                 losses++;
                 guessesLeft--;
             }
-        for (var j=0; j<psychicChoice.length; j++){
 
-            if (userGuess !== psychicChoice) {
-                guessesLeft--;
-                var wrongGuess = [""];
-                wrongGuess.unshift(userGuess);
+            if (userGuess !== psychicChoice) {             
+                wrongGuess.unshift(" " + userGuess);
             }
         }
-            
+        //Stops or resets game when you win or lose
+        if ((guessesLeft == 0) || (losses == 20)) {
+            alert("You lost");
+            reset();
+        } else if (matches == 1) {
+            alert("You won!");
+            reset();
+        }
 
             //HTML input
             var html = 
             "<p>You chose: " + userGuess + "</p>" +
             "<p>The psychic chose: " + psychicChoice + "</p>" +
-            "<p>Matches: " + matches + "</p>" +
-            "<p>Losses: " + losses + "</p>" +
-            "<p>Guesses: " + wrongGuess + "</p>";
+            "<p>Your guesses: " + wrongGuess + "</p>"+
+            "<p>Guesses left: " + guessesLeft + "</p>";
+
 
             document.querySelector("#game").innerHTML = html;
     };
